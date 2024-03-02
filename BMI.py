6b0f74eb8c8995eaffb60a8681dc7496b58b8df5
22203004 <119407153+22203004@users.noreply.github.com>
2024-03-02 23:43:21 +0530

    Add files via upload
    help to check your bmi

---
+++ BMI.py
@@ -0,0 +1,8 @@
+
+height=input("ENTER YOUR HEIGHT in m^2: ")
+weight=input("ENTER YOUR WEIGHT in kg: ")
+h=float(height)
+w=int(weight)
+bmi=(w / h**2 )
+b=int(bmi)
+print(f"your Body Mass index is: {b}")
